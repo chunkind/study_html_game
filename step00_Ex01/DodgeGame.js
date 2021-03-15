d3c3ca23db39da9790eb85ade819206eef7b370b
chunkind <chunkind88@gmail.com>
2021-03-15 18:03:59 +0900

    no message

---
+++ step00_Ex01/DodgeGame.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", drawScreen, false);
+
+function drawScreen(){
+    var theCanvas = document.getElementById("GameCanvas");
+    var Context = theCanvas.getContext("2d");
+}
